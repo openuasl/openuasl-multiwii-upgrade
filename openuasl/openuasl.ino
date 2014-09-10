43592ddafbc9d6d970ce662a1a2dfcea687b0d18
JungHak,Oh <xormcity@naver.com>
2014-09-10 23:32:18 +0900

    firtst commit
    multiwii기반의 openusl
    board spec
    - GY-86
    - HC-SR04 sonar
    - atmega2560
    - ftdi usb serial
    
    기존의 multiwii 기능에 hc-sr04 소나 센서를 이용한 저고도 비행 제어가 가능하도록 기능 추가.
    serial 통신으로 컨트롤이 가능 하도록 수정. (rcserialcnt 변경)

---
+++ openuasl/openuasl.ino
@@ -0,0 +1,13 @@
+/*
+*
+* opensource multiwii ( http://www.multiwii.com ) 기반의
+*
+* openuasl ( https://github.com/openuasl/openuasl-multiwii-upgrade )
+* 
+* 연관된 관련 프로젝트
+* - openuasl ( https://github.com/openuasl/openuasl-resq-android )
+* - openuasl ( https://github.com/openuasl/openuasl-server )
+* - openuasl ( https://github.com/openuasl/openuasl-uav-commbd )
+* - openuasl ( https://github.com/openuasl/openuasl-surv-android )
+*
+*/
